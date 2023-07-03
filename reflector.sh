2554c7bda17e22f0648037e0231c7f91456bc699
Tiz <taikikku3291@icloud.com>
2023-07-03 11:05:10 +0900

    Small Updates

--- reflector.sh
+++ reflector.sh
@@ -1,8 +1,5 @@
 # Use this if you are extremely lazy.
 
-
-
-
 cat build_files.txt | while read line 
 do
    pandoc --standalone src/$line.md -o $line.html
@@ -15,7 +12,7 @@ if [$1];
 then
     git commit -m $1
 else
-    git commit -m 'a small update'
+    git commit -m 'Small Updates'
 fi
 
 git push origin main
