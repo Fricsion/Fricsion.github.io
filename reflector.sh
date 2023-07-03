fe3d375d3b6d850d1d20cc91ed00f6e8eb70fe65
Tiz <tizberius@icloud.com>
2023-07-03 11:23:06 +0900

    Small Updates

--- reflector.sh
+++ reflector.sh
@@ -1,10 +1,10 @@
 # Use this if you are extremely lazy.
 
-cat build_files.txt | while read line 
-do
-   pandoc --standalone src/$line.md -o $line.html
-done
 
+
+for file in src/*; do
+    pandoc --standalone src/$file.md -o $file.html
+done
 
 git add -A
 
