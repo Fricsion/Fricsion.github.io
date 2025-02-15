cbdb292e9cf6ec4af9c62fabec6105291ca56af5
Taiki M <35601059+Fricsion@users.noreply.github.com>
2025-02-15 23:30:21 +0900

    init commit

--- builder.sh
+++
@@ -1,9 +0,0 @@
-# Chmod to allow this file to be run.
-# Edit this file every time you add a new page in src/
-# ./builder.sh
-
-cat build_files.txt | while read line 
-do
-   pandoc --standalone src/$line.md -o $line.html
-done
-
